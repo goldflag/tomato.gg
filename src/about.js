c34769396426605ebc579281d8a6308af5124a48
Goldflag <bill2000yang@gmail.com>
2020-08-26 22:20:06 -0400

    changed about

--- src/about.js
+++ src/about.js
@@ -25,19 +25,18 @@ export default function TankStatsPage(props) {
           <div style={{padding: '1rem', color: 'rgb(50,50,50)'}}>
             <span style={{fontSize: '1.5rem', fontWeight: '500', lineHeight: '3rem'}}>About Tomato.gg</span><br/>
             <span style={{fontSize: '0.8rem', lineHeight: '1.3rem', color: 'rgb(100,100,100)'}}>UPDATED 8/26/2020</span> <br/>
-            <span style={{fontSize: '0.9rem', lineHeight: '1.4rem'}}>I created this site to provide an accessible interface to view a huge variety of stats. Many (but not all) of the stats on tomato.gg are readily available on other sites, 
-            but I've never seen an easy-to-use website that covers all of the most important stats<br/>
+            <span style={{fontSize: '0.9rem', lineHeight: '1.4rem'}}>I created this site to provide an accessible interface to view a huge variety of stats, many of which are only found here. Some stats have taken inspiration from the other sites.<br/>
             Some of the unique features offered on this site:
             <ul>
                 <li>Period breakdown for recent stats - inspired by Wotlabs.net</li>
                 <li>Player MoE data - inspired by the Mastery Badge breakdown on Wotstats.org</li>
                 <li>Player WN8 heatmap - inspired by WN8lab.com and Noobmeter.net</li>
-                <li>Ultra detailed server stats- inspired by vbaddict.net (RIP)</li>
+                <li>Ultra detailed server stats - inspired by vbaddict.net (RIP)</li>
                 <li>Tank WR and WN8 curves - inspired by WR curves on wot-news.com</li>
                 <li>Per-tank DPG and WN8 percentiles</li>
             </ul>
 
-            Tomato.gg doesn't aim to do everything. It will never have server stats as detailed as those on wot-news.com, nor will it have the percise individual battle stats that WN8lab.com. If you want to see detailed period based winrate curves, go to wot-news.com<br/><br/>
+            Tomato.gg doesn't aim to do everything. It will never have server stats as detailed as those on wot-news.com, nor will it have the percise individual battle accessible on WN8lab.com.
 
             Tomato.gg is still in the early stages of development and is nowhere near feature complete. In the near future, I plan to expand the player stats page, expand the tank stats page, implement clan stats, and add tank stats for the EU, ASIA, and RU servers.<br/><br/>
 
