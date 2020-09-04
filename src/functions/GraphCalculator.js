bc7478d6217e678a6b1e6ad6550cec1f641e37b9
Goldflag <bill2000yang@gmail.com>
2020-09-04 13:52:19 -0400

    why

--- src/functions/GraphCalculator.js
+++ src/functions/GraphCalculator.js
@@ -262,11 +262,11 @@ function clr(recent, overall, flipped) {
 }
 
 export default function GraphCalculator(stats, OS, overallWN8, avgTier, recentStats) {
-    //const recentsession = calculateRecents(recentStats.recent24hr, recentStats.overall);
-    const recentsession = calculateRecents(recentStats.recentsession, recentStats.overall);
+    const recentsession = calculateRecents(recentStats.recent24hr, recentStats.overall);
+    //const recentsession = calculateRecents(recentStats.recentsession, recentStats.overall);
     const recent24hr = calculateRecents(recentStats.recent24hr, recentStats.overall);
-    //const recent3days = calculateRecents(recentStats.recent1week, recentStats.overall);
-    const recent3days = calculateRecents(recentStats.recent3days, recentStats.overall);
+    const recent3days = calculateRecents(recentStats.recent1week, recentStats.overall);
+    //const recent3days = calculateRecents(recentStats.recent3days, recentStats.overall);
 
     const recent1week = calculateRecents(recentStats.recent1week, recentStats.overall);
     const recent30days = calculateRecents(recentStats.recent30days, recentStats.overall);
