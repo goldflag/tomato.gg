0845403732b0af6d7e09753428cc9a53e94fb3e2
Goldflag <bill2000yang@gmail.com>
2020-11-29 13:43:03 -0500

    t

--- src/material/searchBar.js
+++ src/material/searchBar.js
@@ -67,7 +67,7 @@ export default function SearchBar(props) {
     <Paper component="form" className={classes.root}>
       <InputBase
         className={classes.input}
-        placeholder="Enter Username (e.g. wingedleader)"
+        placeholder="Enter Username (e.g. Goldflag)"
         inputProps={{ 'aria-label': 'search google maps' }}
         onChange={e => props.setName(e.target.value)}
       />      
