713f72ea576bc36691e09284c0cef1f6166caf87
Elijah Sink <theminorprofit@gmail.com>
2021-01-04 21:41:19 -0600

    Fix import mistake

--- src/components/info.js
+++ src/components/info.js
@@ -1,4 +1,4 @@
-import { styled, css } from "styled-components";
+import styled, { css } from "styled-components";
 export const Info = styled.div`
     background-color: ${({ theme }) =>
         theme === "dark" ? css`rgb(40, 40, 40)` : css`rgb(250, 250, 250)`};
