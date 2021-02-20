1b6a074ae8bfa7e5649c4a031375d15fbf55f2e1
Elijah Sink <theminorprofit@gmail.com>
2021-02-20 16:58:07 -0600

    Refactor treemap.js

--- src/statpages/statsPageComponents/treemap/treemap.js
+++ src/statpages/statsPageComponents/treemap/treemap.js
@@ -1,64 +1,53 @@
 import React, { useState } from "react";
 import TreemapOverall from "./treemapOverall";
-import { ThemeContext } from "../../../context";
 import {
     FilterButtonGroup,
     FilterButton,
 } from "../../../components/tableFilters";
 import styled from "styled-components";
 
-const Styles = styled.div`
+const MaxHeight = styled.div`
     height: "900px";
-    .filters {
-        margin-bottom: -10px;
-    }
 `;
 
-export default function Treemap(props) {
-    const { theme } = React.useContext(ThemeContext);
+const Filters = styled(FilterButtonGroup)`
+    margin-bottom: 0px;
+`;
 
-    const [data, setData] = useState(props.data.overall);
-    const [type, setType] = useState("Overall");
+const filters = [
+    {
+        label: "Overall",
+        key: "overall",
+    },
+    {
+        label: "30 Days",
+        key: "recent30",
+    },
+    {
+        label: "60 Days",
+        key: "recent60",
+    },
+];
+export default function Treemap({ data }) {
+    const [filterValue, setFilterValue] = useState(filters[0]);
 
     return (
-        <Styles>
-            <div className="filters">
-                    <FilterButtonGroup>
-                        <FilterButton
-                            key={"overall"}
-                            selected={type === "Overall"}
-                            onClick={() => {
-                                setData(props.data.overall);
-                                setType("Overall");
-                            }}
-                        >
-                            Overall
-                        </FilterButton>                
-                        <FilterButton
-                            key={30}
-                            selected={type === "30 Days"}
-                            onClick={() => {
-                                setData(props.data.recent30);
-                                setType("30 Days");
-
-                            }}
-                        >
-                            30 Days
-                        </FilterButton>  
-                        <FilterButton
-                            key={60}
-                            selected={type === "60 Days"}
-                            onClick={() => {
-                                setData(props.data.recent60);
-                                setType("60 Days");
-
-                            }}
-                        >
-                            60 Days
-                        </FilterButton>   
-                    </FilterButtonGroup>
-                </div>
-            <TreemapOverall data={data} type={type}/>
-        </Styles>
+        <MaxHeight>
+            <Filters>
+                {filters.map(({ label, key }) => (
+                    <FilterButton
+                        key={key}
+                        selected={filterValue.key === key}
+                        onClick={() => setFilterValue({ label, key })}
+                    >
+                        {label}
+                    </FilterButton>
+                ))}
+            </Filters>
+            <TreemapOverall
+                data={data[filterValue.key]}
+                type={filterValue.label}
+            />
+        </MaxHeight>
     );
 }
