d67262e56b61f0d4537fbd7d4ffc862577280234
Elijah Sink <theminorprofit@gmail.com>
2021-03-10 01:38:53 -0600

    Make button colors match search items

--- src/topbar/searchModeSelect.js
+++ src/topbar/searchModeSelect.js
@@ -6,6 +6,7 @@ import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 // LOCAL
 import { SearchmodeContext } from "Context";
+import { commonStrings } from "Data/localizations";
 
 const ButtonWrapper = styled.div`
     margin-left: 0.5rem;
@@ -20,21 +21,20 @@ const SelectButton = styled(Button)`
     font-weight: 500 !important;
     color: rgb(210, 210, 210) !important;
     background-color: rgb(37, 46, 105) !important;
-    border: 2px solid ${({ selected }) => (selected ? "rgb(207, 31, 242)" : "rgba(0, 0, 0, 0)")}  !important;
-    border-radius: ${({ radius }) => radius} !important;
+    border: 2px solid ${({ selected, borderColor }) => (selected ? borderColor : "rgba(0, 0, 0, 0)")} !important;
+    border-radius: ${({ borderRadius }) => borderRadius} !important;
     border-right: 0;
     padding: 1px 0.5rem !important;
     width: 80px;
     &:hover {
         color: rgb(255, 255, 255) !important;
-        border: 2px solid rgba(150, 150, 200, 1) !important;
+        border-color: rgba(150, 150, 200, 1) !important;
     }
 `;
 
-const modes = ["player", "clan"];
-const radiuses = [
-    "20px 0 0 20px",
-    "0 20px 20px 0"
+const modes = [
+    { value: "player", label: commonStrings.player, borderRadius: "20px 0 0 20px", borderColor: "#4e80c2" },
+    { value: "clan", label: commonStrings.clan, borderRadius: "0 20px 20px 0", borderColor: "rgb(141, 86, 232)" },
 ];
 
 const ServerSelectButtons = () => {
@@ -42,9 +42,15 @@ const ServerSelectButtons = () => {
     return (
         <ButtonWrapper>
             <ButtonGroup variant="text">
-                {modes.map((id, i) => (
-                    <SelectButton key={i} radius={radiuses[i]} selected={mode === id} onClick={() => setMode(id)}>
-                        {id}
+                {modes.map(({ value, label, borderRadius, borderColor }, i) => (
+                    <SelectButton
+                        key={i}
+                        borderRadius={borderRadius}
+                        borderColor={borderColor}
+                        selected={mode === value}
+                        onClick={() => setMode(value)}
+                    >
+                        {label}
                     </SelectButton>
                 ))}
             </ButtonGroup>
