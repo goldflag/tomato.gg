039298df2710974ce8876f1b56bf3872e48e6af7
goldflag <billyang@umich.edu>
2021-05-02 18:40:58 -0400

    added scrollbar for all players on search suggestions

--- src/functions/searchFunctions.js
+++ src/functions/searchFunctions.js
@@ -1,10 +1,12 @@
 import React from "react";
 import FuzzySet from 'fuzzyset'
 import styled from "styled-components";
+import Scrollbar from "react-scrollbars-custom";
+
 const APIKey = process.env.REACT_APP_API_KEY2;
 
 const Button = styled.button`
-    max-width: 700px;
+    width: 100%;
     padding: 10px 14px;
     font-size: 1rem;
     color: white;
@@ -24,6 +26,11 @@ const OptionsMeta = styled.span`
     padding: 5px 14px;
 `;
 
+const All = styled.div`
+    height: calc(250px);
+    background-color: rgb(40, 40, 70);
+`;
+
 export function getSuggestionsFromHistory(name) {
     const searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
     const fuzz = FuzzySet();
@@ -32,7 +39,6 @@ export function getSuggestionsFromHistory(name) {
     }
     return fuzz.get(name);
 }
-
 
 export async function searchNames(name, time, setData) {
     const currenttime = Date.now();
@@ -48,20 +54,18 @@ export function options(name, setName, data) {
     if (data) {
         const searchSuggestions = getSuggestionsFromHistory(name) || [];
         const history = searchSuggestions.map(([_, name]) => ({ nickname: name }));
-        const all = data.slice(0, 8);
 
         const fromHistory = history.map(({ nickname }, i) => {
-                if (i < 5) {
-                    return (
-                        <Button key={nickname} onClick={() => setName(nickname)} type="submit">
-                            {nickname}
-                        </Button>
-                    );
-                }
+                return ( 
+                    i < 5 ?                         
+                    <Button key={nickname} onClick={() => setName(nickname)} type="submit">
+                        {nickname}
+                    </Button>
+                    : null)
             }            
         );
 
-        const fromAll = all.map(({ nickname }) => {
+        const fromAll = data.map(({ nickname }) => {
                 return (
                     <Button key={nickname} onClick={() => setName(nickname)} type="submit">
                         {nickname}
@@ -74,8 +78,13 @@ export function options(name, setName, data) {
             <>
                 {history.length > 0 ? <OptionsMeta>From your history</OptionsMeta> : null}
                 {fromHistory}
-                {all.length > 0 ? <OptionsMeta>All players</OptionsMeta> : null}
-                {fromAll}
+                {data.length > 0 ? <OptionsMeta>All players</OptionsMeta> : null}
+                <All>
+                    <Scrollbar>
+                            {fromAll}
+                    </Scrollbar>
+                </All>
+
             </>
         );
     }
